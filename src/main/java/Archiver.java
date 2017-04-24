719e55ab55c5adc75de6e3b53c3022fddf8176f4
Egor <gvozdetscky.egor@yandex.ru>
2017-04-24 12:16:22 +0400

    Довел до ума

--- src/main/java/Archiver.java
+++ src/main/java/Archiver.java
@@ -3,46 +3,56 @@ import java.util.List;
 
 public class Archiver {
 
-    private static byte[] compressed(byte[] buffer) {
+    public static byte[] compressed(byte[] buffer) {
+        return compressedRLE(buffer);
+    }
+
+    public static byte[] deCompressed(byte[] buffer) {
+        return deCompressedRLE(buffer);
+    }
+
+    private static byte[] compressedRLE(byte[] buffer) {
         List<Byte> compressed = new ArrayList<>();
         List<Byte> unequalSequence = new ArrayList<>();
-        int count = 1;
-        int anotherCount = 0;
+        int countEqualValues = 1;
+        int countUnequalValues = 1;
         for (int i = 0; i < buffer.length - 1; i++) {
             if (i < buffer.length && buffer[i] == buffer[i + 1]) {
-                count++;
-                if (count == 128) {
-                    compressed.add((byte) (count -1));
+                countEqualValues++;
+                if (countEqualValues == 128) {
+                    compressed.add((byte) (countEqualValues -1));
                     compressed.add(buffer[i]);
-                    count = 1;
+                    countEqualValues = 1;
                 }
-                if (anotherCount > 1) {
-                    compressed.add((byte) ((anotherCount - 1) * -1));
+                if (countUnequalValues > 1) {
+                    compressed.add((byte) ((countUnequalValues - 1) * -1));
                     compressed.addAll(unequalSequence);
                 }
-                anotherCount = 0;
+                countUnequalValues = 0;
                 unequalSequence.clear();
             } else if (i < buffer.length && buffer[i] != buffer[i + 1]) {
-                anotherCount++;
-                if (anotherCount != 1)
+                countUnequalValues++;
+                if (countUnequalValues != 1)
                     unequalSequence.add(buffer[i]);
-                if (anotherCount == 128) {
-                    compressed.add((byte) (anotherCount -1));
+                if (countUnequalValues == 128) {
+                    compressed.add((byte) ((countUnequalValues - 1) * -1));
+                    compressed.addAll(unequalSequence);
+                    unequalSequence.clear();
+                    countUnequalValues = 1;
+                }
+                if (countEqualValues != 1) {
+                    compressed.add((byte) (countEqualValues));
                     compressed.add(buffer[i]);
                 }
-                if (count != 1) {
-                    compressed.add((byte) (count));
-                    compressed.add(buffer[i]);
-                }
-                count = 1;
+                countEqualValues = 1;
             }
         }
-        if (count != 1) {
-            compressed.add((byte) (count));
+        if (countEqualValues != 1) {
+            compressed.add((byte) (countEqualValues));
             compressed.add(buffer[buffer.length - 1]);
         }
-        if (anotherCount > 0) {
-            compressed.add((byte) (anotherCount * -1));
+        if (countUnequalValues > 0) {
+            compressed.add((byte) (countUnequalValues * -1));
             compressed.addAll(unequalSequence);
             compressed.add(buffer[buffer.length - 1]);
         }
@@ -50,7 +60,7 @@ public class Archiver {
         return toArrayFromList(compressed);
     }
 
-    private static byte[] deCompressed(byte[] buffer) {
+    private static byte[] deCompressedRLE(byte[] buffer) {
         List<Byte> decompressed = new ArrayList<>();
         int iter = 0;
         while(iter < buffer.length) {
@@ -79,14 +89,21 @@ public class Archiver {
     }
 
     public static void main(String[] args) {
-        String example = "aaaaaaaazxcvbnmmnbvbbbb11112345678911111ooo123456789LL8";
+        String example = "01234567890123456789012345678901234567890123456789" +
+                "01234567890123456789012345678901234567890123456789" +
+                "0123456789012345678901234567" +
+                "aa1aaaaaazxcvbnmmnbvbbbb11112345678911111ooo123456789LL8";
         byte[] compressed = compressed(example.getBytes());
         for (byte b :
                 compressed) {
             System.out.print(b + ".");
         }
         System.out.println();
-        System.out.println(new String(deCompressed(compressed)));
+        String result = new String(deCompressed(compressed));
+        System.out.println(result);
+        if (example.equals(result)) {
+            System.out.println("Получилось");
+        }
     }
 
     private static byte[] toArrayFromList(List<Byte> list) {
